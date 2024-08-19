786ea0a206181b7b33e3a042999efc2af2b303e0
Luiz <l.eduardo8316@gmail.com>
2024-08-19 20:27:09 -0300

    refactor: spy validate method

--- src/domain/enterprise/value-objects/uuid.vo.spec.ts
+++ src/domain/enterprise/value-objects/uuid.vo.spec.ts
@@ -1,17 +1,26 @@
 import { randomUUID } from "crypto"
 import { InvalidUuidError, Uuid } from "./uuid.vo"
+import { MockInstance } from 'vitest'
 
 describe('Uuid unit tests', () => {
+    let validateSpy: MockInstance<any>
+    beforeEach(() => {
+        validateSpy  = vi.spyOn(Uuid.prototype as any, 'validate')
+
+    })
+
     it('should throw error when uuid is invalid', () => {
         expect(() => {
             new Uuid('invalid-uuid')
         }).toThrowError(new InvalidUuidError())
+        expect(validateSpy).toHaveBeenCalledTimes(1)
     })
 
     it('should be able to create a valid uuid', () => {
       const uuid = new Uuid()
       
       expect(uuid).toBeInstanceOf(Uuid)
+      expect(validateSpy).toHaveBeenCalledTimes(1)
     })
 
     it('should be able to accept a valid uuid', () => {
@@ -19,5 +28,6 @@ describe('Uuid unit tests', () => {
         const uuid = new Uuid(existsId)
 
         expect(uuid.id).toEqual(existsId)
+        expect(validateSpy).toHaveBeenCalledTimes(1)
     })
 })
