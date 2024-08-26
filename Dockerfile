add924fffec330b2136d3c421a8811e635a7c36c
Luiz <l.eduardo8316@gmail.com>
2024-08-26 20:37:10 -0300

    feat: add configuration in docker compose and create entity abstract class

--- Dockerfile
+++ Dockerfile
@@ -1,28 +1,39 @@
 FROM node:22-slim
 
-# Atualiza os pacotes e instala as dependências
 RUN apt-get update && \
     apt-get install -y \
     gnupg \
     curl \
     lsb-release \
-    apt-transport-https
+    apt-transport-https \
+    tar \
+    && apt-get clean && \
+    rm -rf /var/lib/apt/lists/*
 
-# Adiciona o repositório oficial do Fish Shell
 RUN curl -fsSL https://download.opensuse.org/repositories/shells:fish:release:3/Debian_11/Release.key | apt-key add - && \
     echo "deb http://download.opensuse.org/repositories/shells:fish:release:3/Debian_11/ /" > /etc/apt/sources.list.d/shells:fish:release:3.list
 
-# Atualiza os pacotes e instala o Fish Shell
 RUN apt-get update && \
     apt-get install -y fish && \
     apt-get clean && \
     rm -rf /var/lib/apt/lists/*
 
-# Define o Fish Shell como shell padrão
-SHELL ["/usr/bin/fish", "-c"]
+# Baixe e instale o Starship
+RUN curl -fsSL https://github.com/starship/starship/releases/latest/download/starship-x86_64-unknown-linux-musl.tar.gz | \
+    tar -xzC /usr/local/bin starship
+
+# Crie diretórios e configure o Starship para o usuário node
+USER root
+
+RUN mkdir -p /home/node/.config/fish && \
+    chown -R node:node /home/node/.config && \
+    echo 'starship init fish | source' > /home/node/.config/fish/config.fish
 
 USER node
 
-WORKDIR  /home/node/app
+# Defina o Fish Shell como o shell padrão
+SHELL ["/usr/bin/fish", "-c"]
+
+WORKDIR /home/node/app
 
 CMD [ "tail", "-f", "/dev/null" ]
--- src/domain/enterprise/entities/category.ts
+++ src/domain/enterprise/entities/category.ts
@@ -1,78 +1,84 @@
-import { CategoryValidatorFactory } from "../validators/category.validator"
-import { EntityValidationError } from "../validators/errors/validation.error"
-import { Uuid } from "../value-objects/uuid.vo"
+import { Entity } from "@/core/entities/entity";
+import { CategoryValidatorFactory } from "../validators/category.validator";
+import { EntityValidationError } from "../validators/errors/validation.error";
+import { Uuid } from "../value-objects/uuid.vo";
+import { ValueObject } from "@/core/entities/value-object";
 
 export type CategoryProps = {
-    id?: Uuid
-    name: string
-    description: string | null
-    isActive: boolean
-    createdAt?: Date
-}
+  id?: Uuid;
+  name: string;
+  description: string | null;
+  isActive: boolean;
+  createdAt?: Date;
+};
 
 export type CategoryCreateCommand = {
-    name: string
-    description: string
-    isActive: boolean
-}
+  name: string;
+  description: string;
+  isActive: boolean;
+};
 
-export class Category {
-    id: Uuid
-    name: string
-    description?: string | null
-    isActive?: boolean
-    createdAt?: Date
+export class Category extends Entity<CategoryProps> {
+  id: Uuid;
+  name: string;
+  description?: string | null;
+  isActive?: boolean;
+  createdAt?: Date;
 
-    constructor(props: CategoryProps) {
-        this.id = props.id ?? Uuid.create()
-        this.name = props.name
-        this.description = props.description
-        this.isActive = props.isActive ?? true
-        this.createdAt = props.createdAt ?? new Date()
+  constructor(props: CategoryProps) {
+    super();
+    this.id = props.id ?? Uuid.create();
+    this.name = props.name;
+    this.description = props.description;
+    this.isActive = props.isActive ?? true;
+    this.createdAt = props.createdAt ?? new Date();
+  }
+
+  get entityId(): ValueObject {
+    return this.id;
+  }
+
+  static create(props: CategoryCreateCommand): Category {
+    const category = new Category(props);
+    Category.validate(category);
+    return category;
+  }
+
+  changeName(name: string): void {
+    this.name = name;
+    Category.validate(this);
+  }
+
+  changeDescription(description: string): void {
+    this.description = description;
+    Category.validate(this);
+  }
+
+  activate() {
+    this.isActive = true;
+  }
+
+  deactivate() {
+    this.isActive = false;
+  }
+
+  static validate(entity: Category) {
+    const validator = CategoryValidatorFactory.create();
+
+    const isValid = validator.validate(entity);
+
+    if (!isValid && validator.errors) {
+      throw new EntityValidationError(validator.errors);
     }
+  }
 
-    static create(props: CategoryCreateCommand): Category {
-        const category = new Category(props)
-        Category.validate(category)
-        return category
-    }
-
-    changeName(name: string): void {
-        this.name = name
-        Category.validate(this)
-    }
-
-    changeDescription(description: string): void {
-        this.description = description
-        Category.validate(this)
-    }
-
-    activate() {
-        this.isActive = true
-    }
-
-    deactivate() {
-        this.isActive = false
-    }
-
-    static validate(entity: Category) {
-        const validator = CategoryValidatorFactory.create()
-
-        const isValid = validator.validate(entity)
-
-        if(!isValid && validator.errors) {
-            throw new EntityValidationError(validator.errors)
-        }
-    }
-
-    toJSON() {
-        return {
-            id: this.id.value,
-            name: this.name,
-            description: this.description,
-            isActive: this.isActive,
-            createdAt: this.createdAt
-        }
-    }
-
+  toJSON() {
+    return {
+      id: this.id.value,
+      name: this.name,
+      description: this.description,
+      isActive: this.isActive,
+      createdAt: this.createdAt,
+    };
+  }
 }
